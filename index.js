610c447bba03f3a8f0ab1607c6b17b258df42d8c
ZJU-FrankGu <2961429602@qq.com>
2022-09-30 20:24:25 +0800

    first

--- index.js
+++ index.js
@@ -1,15 +1,7 @@
-
-let w = 260;
-let h = 400;
-let score = 0;
-var begin;
-//获取绘画环境
-let c=document.getElementById('canvas');
-let canvas=c.getContext('2d');
-// function go(){
-//     begin = true;
-//     // running = setInterval("goGame();", 10);
-// }
+var w = 260;
+var h = 400;
+var score = 0;
+var div = document.getElementById("score");
 
 function Rect(y, arr) {
     //每一个rect表示一行
@@ -20,40 +12,41 @@ function Rect(y, arr) {
     this.arr = arr;
 }
 
-// 用二维矩阵存储格子的情况
+var game = document.getElementById("game");
+var canvas = game.getContext("2d");
+game.width = w;
+game.height = h;
+
+canvas.fillStyle = "#000000";
+//a里面放入6个Rect
 var a = new Array();
-for (var i=0;i<6;i++)
-{
+for (var i = 0; i < 6; i++) {
+    //每一个Rect里面要有一个y，一个arr
     var arr = new Array();
-    for (var j=0;j<4;j++){
+    for (var j = 0; j < 4; j++) {
         arr[j] = 0;
     }
+    //应该随机一个让它是黑色
     var n = Math.floor(Math.random() * 4);
-    arr[n] = 1; // 为1代表要染色的格子
-    a[i] = new Rect(100*i-200,arr);
+    arr[n] = 2;
+    a[i] = new Rect(i * 100 - 200, arr);
 }
 
-function drawGame() {
-    for (var i = 0; i < a.length; i++) {
-        for (var j = 0; j < a[i].arr.length; j++) {
-            //颜色是什么？
-            if (a[i].arr[j]==0)
-                canvas.fillStyle = "white";
-            if (a[i].arr[j]==1)
-                canvas.fillStyle = "black";
-            if (a[i].arr[j]==2) // 点错了
-                canvas.fillStyle = "red";
-            if (a[i].arr[j]==3)
-                canvas.fillStyle = "green";
-            canvas.fillRect(j * (w / 4), a[i].y, (w / 4), (h / 4));
-            //描边
-            canvas.strokeStyle = "#666666";
-            canvas.strokeRect(j * (w / 4), a[i].y, (w / 4), (h / 4));
-        }
-    }
-    canvas.strokeStyle = "#000000";
-    canvas.strokeRect(0, 0, w, h);
-}
+
+var backing = null;
+var backsum = 0;
+//回退的方法
+// function goBack() {
+//     if (backsum == h / 4) {
+//         //停止回退
+//         clearInterval(backing);
+//     }
+//     for (var i = 0; i < a.length; i++) {
+//         a[i].y--;
+//     }
+//     drawGame();
+//     backsum++;
+// }
 
 //能动的方法
 function goGame() {
@@ -62,70 +55,144 @@ function goGame() {
         a[i].y++;
         if (a[i].y > h) {
             // for (var j = 0; j < a[i].arr.length; j++) {
-            // //     if (a[i].arr[j] == 2) {
-            // //         //gameover
-            // //         // isrunning = false;
-            // //         clearInterval(running);
-            // // //         a[i].arr[j] = 1;
-            // // //         //回退
-            // // //         // backing = setInterval("goBack();", 10);
-            // //         // return;
-            // //     }
+            //     if (a[i].arr[j] == 2) {
+            //         //gameover
+            //         isrunning = false;
+            //         clearInterval(running);
+            //         a[i].arr[j] = 1;
+            //         //回退
+            //         // backing = setInterval("goBack();", 10);
+            //         // return;
+            //     }
+
             // }
 
             //出去了，就再利用
             for (var j = 0; j < a[i].arr.length; j++) {
                 a[i].arr[j] = 0;
+
             }
             var n = Math.floor(Math.random() * 4);
-            a[i].arr[n] = 1;
+            a[i].arr[n] = 2;
             //放到最上面去
             a[i].y -= h / 4 * 6;
+            // 所以不一定最下面的一行是a[5]
+            // 而是a[5]到最下面了，就修改y让他到上面去
+        }
+
+
+    }
+
+
+}
+//绘制的方法
+
+function drawGame() {
+    for (var i = 0; i < a.length; i++) {
+        for (var j = 0; j < a[i].arr.length; j++) {
+            //颜色是什么？
+            switch (a[i].arr[j]) {
+                case 0:
+                    canvas.fillStyle = "#ffffff";
+                    break;
+                case 1:
+                    canvas.fillStyle = "#ff0000";
+                    break;
+                case 2:
+                    canvas.fillStyle = "#000000";
+                    break;
+                case 3:
+                    canvas.fillStyle = "#dddddd";
+                    break;
+                default:
+                    canvas.fillStyle = "#dddddd";
+            }
+            //画
+            canvas.fillRect(j * (w / 4), a[i].y, (w / 4), (h / 4));
+            //描边
+            canvas.strokeStyle = "#666666";
+            canvas.strokeRect(j * (w / 4), a[i].y, (w / 4), (h / 4));
+
+
+        }
+
+
+    }
+    canvas.strokeStyle = "#000000";
+    canvas.strokeRect(0, 0, w, h);
+
+
+}
+
+
+function move()
+{   
+    /*
+    最满意的一段代码，想了好久，最后实现了
+    这里不能换y！！！
+    本质就是把下一行的数据换成上一行的，实现下落的效果
+    换了y等于啥也没动
+    */
+    // let p = a[5].y;
+    // let t = a[5].arr;
+    for (let i=5;i>=1;i--)  // 倒着来
+    {
+        // a[i].y = a[i-1].y;
+        a[i].arr = a[i-1].arr;
+
+    }
+    // 一开始我想直接把a[5]赋给a[0]
+    // 但后来发现会重复，a[0]要重现初始化
+    let p =a[0].y;
+    var arr = new Array();
+    for (var j = 0; j < 4; j++) {
+        arr[j] = 0;
+    }
+    var n = Math.floor(Math.random() * 4);
+    arr[n] = 2;
+    a[0]  = new Rect(p,arr);
+    // a[0].arr = t;
+}
+
+var begin = true;
+function onGamedown(event) {
+    if (isrunning) {
+        event = event || window.event;
+        var x = event.clientX - 200;
+        var y = event.clientY - 100;
+
+        //判断点击的是哪个矩形
+        for (var i = 0; i < a.length; i++) {
+            if (a[i].y < y && a[i].y + h / 4 > y) {
+                //点击在了第i行
+                var j = Math.floor(x / (w / 4));
+                //将上面取整的数加1
+                a[i].arr[j]++;
+
+                if (a[i].arr[j] == 1) {
+                    drawGame();
+                    clearInterval(running);
+                    isrunning = false;
+                    alert("点错啦！");
+                    return;
+                } else if (a[i].arr[j] == 3) {
+                    score++;
+                    a[i].arr[j] = 2;    // 关键代码！不然一轮完就变会=灰了
+                    move();
+                    drawGame();
+                    div.innerHTML = "得分：" + score;
+                    
+                }
+
+
+            }
+
+
         }
     }
-}
 
-// for (let k = 0;k<4;k++)
-//     {
-//         if (a[a.length].arr[k] == 1)
-//         {
-//             break;  
-//         }
-//     }
-window.onkeydown = function(e){ //event可以简写成 e
 
-    // 此时 k 表示第k个轨道需要被点击
-    if (e.key == "g"){  // 按g键开始游戏
-        running = setInterval("goGame();", 10);
-    }
-    if (e.key == "h"){  // 按h键暂停游戏
-        clearInterval(running);
-    }
-    var m = -1;
-    if (e.key == "s")
-            m = 0;
-    if (e.key == "d") // d
-        m = 1;
-    if (e.key == "j") // j
-        m = 2;
-    if (e.key == "k") // k
-        m = 3;
-    // for (var i = 0; i < a.length; i++) {
-    a[5].arr[m] +=2;
-    if (a[5].arr[m] == 3)
-    {
-        score++;
-        div.innerHTML = "得分：" + score;
-    }
-    else if (a[5].arr[m] == 2){
-        // drawGame();
-        clearInterval(running);
-        alert("点错啦 \n 按g重新开始~");
-    }
-    // if (m = k)
-    //     score++;
-    // else
-    //     clearInterval(running);
-    // if (m!=-1)
-    // alert(m);
 }
+var isrunning = true;
+var running = null;
+drawGame();
