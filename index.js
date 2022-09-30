26841d2801831aeecf0c8ae0fc4be09bd64836e7
ZJU-FrankGu <2961429602@qq.com>
2022-09-30 22:38:03 +0800

    last

--- index.js
+++ index.js
@@ -32,22 +32,6 @@ for (var i = 0; i < 6; i++) {
     a[i] = new Rect(i * 100 - 200, arr);
 }
 
-
-var backing = null;
-var backsum = 0;
-//回退的方法
-// function goBack() {
-//     if (backsum == h / 4) {
-//         //停止回退
-//         clearInterval(backing);
-//     }
-//     for (var i = 0; i < a.length; i++) {
-//         a[i].y--;
-//     }
-//     drawGame();
-//     backsum++;
-// }
-
 //能动的方法
 function goGame() {
     drawGame();
@@ -153,26 +137,96 @@ function move()
     a[0]  = new Rect(p,arr);
     // a[0].arr = t;
 }
-
+function down()
+{   
+    max = 0;
+    for (let j = 0;j<a.length;j++)
+    {
+        if (a[j].y>a[max].y)
+            max = j;
+    }
+    return max;
+}
 var begin = true;
+var isrunning = true;
+var running = null;
+var istiming = true;
+window.onkeydown = function(e){ //event可以简写成 e
+       // 计时
+       var m = -1;
+       if (e.key == "s")
+           m = 0;
+       else if (e.key == "d") // d
+           m = 1;
+       else if (e.key == "j") // j
+           m = 2;
+       else if (e.key == "k") // k
+           m = 3;
+       if (istiming)
+       {
+           start();
+           istiming = false;
+       }
+       if (isrunning) {
+        //    event = event || window.event;
+        //    var x = event.clientX - 200;
+        //    var y = event.clientY - 100;
+           // var bbox =game.getboundingclientrect();
+           // x=x- bbox.left *(game.width / bbox.width);
+           // y=y - bbox.top * (game.height / bbox.height);
+           //判断点击的是哪个矩形
+           // for (var i = 0; i < a.length; i++) {
+           let i = down();
+               if (0<=m && m<=3) {
+                   //点击在sdjk其中之一
+                   a[i].arr[m]++;
+                   if (a[i].arr[m] == 1) {
+                       drawGame();
+                       clearInterval(running);
+                       isrunning = false;
+                       clearInterval(times);
+                       boxObj.innerHTML = num;
+                       alert("点错啦！");
+                       return;
+                   } else if (a[i].arr[m] == 3) {
+                       score++;
+                       a[i].arr[m] = 2;    // 关键代码！不然一轮完就变会=灰了
+                       move();
+                       drawGame();
+                       div.innerHTML = "得分：" + score;   
+                   }
+               }
+   
+           }
+}
 function onGamedown(event) {
+    // 计时
+    if (istiming)
+    {
+        start();
+        istiming = false;
+    }
     if (isrunning) {
         event = event || window.event;
         var x = event.clientX - 200;
         var y = event.clientY - 100;
-
+        // var bbox =game.getboundingclientrect();
+        // x=x- bbox.left *(game.width / bbox.width);
+        // y=y - bbox.top * (game.height / bbox.height);
         //判断点击的是哪个矩形
-        for (var i = 0; i < a.length; i++) {
+        // for (var i = 0; i < a.length; i++) {
+        let i = down();
             if (a[i].y < y && a[i].y + h / 4 > y) {
                 //点击在了第i行
                 var j = Math.floor(x / (w / 4));
                 //将上面取整的数加1
                 a[i].arr[j]++;
-
                 if (a[i].arr[j] == 1) {
                     drawGame();
                     clearInterval(running);
                     isrunning = false;
+                    clearInterval(times);
+                    boxObj.innerHTML = num;
                     alert("点错啦！");
                     return;
                 } else if (a[i].arr[j] == 3) {
@@ -180,19 +234,40 @@ function onGamedown(event) {
                     a[i].arr[j] = 2;    // 关键代码！不然一轮完就变会=灰了
                     move();
                     drawGame();
-                    div.innerHTML = "得分：" + score;
-                    
+                    div.innerHTML = "得分：" + score;   
                 }
-
-
             }
-
 
         }
     }
 
-
-}
-var isrunning = true;
-var running = null;
 drawGame();
+
+// 1.获取节点
+var boxObj = document.querySelector('#box');
+// 2.设置变量
+// 保存定时器返回值,方便后面清空
+var times = '';
+// 保存页面中起始数据
+var num = boxObj.innerHTML;
+// 3.点击开始，实现倒计时计数
+function start() {
+    // 4.设置定时器
+    times = setInterval(function () {
+        var num1 = boxObj.innerHTML;
+        // 5.将数字减一后，返回
+        num1--;
+        boxObj.innerHTML = num1;
+        // 6.数字小于0，停止计时器，数字设置为起始数字
+        if (num1 < 0) {
+            clearInterval(times);
+            boxObj.innerHTML = num;
+            var str;
+            if (score>50)
+                str = "太强了！";
+            else if (score<10)
+                str = "加把劲哦~";
+            alert("游戏时间到！ \n你的得分是"+score+"分!"+str);
+        }
+    }, 1000);
+}
